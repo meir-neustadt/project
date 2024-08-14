d7f829172e08d0f06347071b39cc97dbd653a902
MeirNeustadt <msn.binah.1@gmail.com>
2024-08-14 12:55:17 +0300

    -

--- Back/cpp/calculator.cpp
+++ Back/cpp/calculator.cpp
@@ -4,6 +4,7 @@
 
 #define IS_NUM(c) ((c>='0') && (c<='9'))
 #define TO_NUM(c) (c-='0')
+#define IS_MLP(c) (c=='*') || (c=='/')
 #define st "  "
 #define PRINTV { std::cout << st; }
 #define PRINT(a) { std::cout << (a); }
@@ -32,9 +33,16 @@ double result(char* expression, int start, int end){
         int num {0};
         if (expression[i] == '[') {
             start = ++i; i = expressionEnd(expression, i);
-            [=](){for (int j {start}; j<i; ++j) {PRINT(expression[j])} }(); PRINTV;
+            //[=](){for (int j {start}; j<i; ++j) {PRINT(expression[j])} }(); PRINTV;
             num = result(expression, start, i);
-        } else if ([expression,i](){return true;}()) {
+        // } else if ([expression, end, i](){
+        //     for (; i<end; ++i) {
+        //         char ex{expression[i]};
+        //         if (!IS_NUM(ex) && !IS_MLP(ex)) return false;
+        //         if (IS_MLP(ex)) {; return true;}
+        //         }
+        //     return false;
+        //     }()) {
         } else {
             for (; i<end && IS_NUM(expression[i]); ++i) num = num*10+TO_NUM(expression[i]);
         }
@@ -68,7 +76,7 @@ int main(int argc, char* argv[]) {
     }
 
     char* expression = argv[1];
-    int ln = [expression](){int i{0}; while (expression[i] != '\0') ++i ;return i;}();
+    int ln = [expression](){int i{0}; while (expression[i++] != '\0') ;return i;}();
     //auto len = [](char* expression){int i{0}; while(expression[i]!='\0')++i ;return i;};
     double res = result(argv[1], 0, ln);
     PRINTLL(res);
--- Front/script.js
+++ Front/script.js
@@ -1,11 +1,46 @@
 let display = document.getElementById("display");
 
-function press(value) {
-    display.value += value;
-}
 
 function clearDisplay() {
     display.value = "";
+}
+
+function eraseOneLeft() {
+    display.value = display.value.slice(0, -1);
+}
+
+function press(value) {
+    // Get the current cursor position
+    let cursorPos = display.selectionStart;
+    
+    // Get the text before and after the cursor
+    let beforeCursor = display.value.slice(0, cursorPos);
+    let afterCursor = display.value.slice(cursorPos);
+
+    // Insert the value at the cursor position
+    display.value = beforeCursor + value + afterCursor;
+    
+    // Move the cursor to the right of the inserted value
+    cursorPos += value.length;
+
+    // Update the cursor position and refocus the input
+    setTimeout(() => {
+        display.setSelectionRange(cursorPos, cursorPos);
+        display.focus();
+    }, 0);
+}
+
+
+function moveOneLeft() {
+    let cursorPos = display.selectionStart;
+
+    // Move the cursor one position to the left if it's not at the beginning
+    if (cursorPos > 0) {
+        display.setSelectionRange(cursorPos - 1, cursorPos - 1);
+    }
+
+    // Focus the input field to ensure the cursor is visible
+    display.focus();
 }
 
 function calculate() {
